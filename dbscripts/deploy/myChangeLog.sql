dcd92905ab75f436fa25c2d58b9e3bcaa82d7504
loicbi <loic.andre.bi@gmail.com>
2023-05-08 01:14:30 -0400

    yml commit develop 23

--- dbscripts/deploy/myChangeLog.sql
+++ dbscripts/deploy/myChangeLog.sql
@@ -1,7 +1,6 @@
 -- liquibase formatted sql
 
 
-
 --changeset a.loic:1 labels:example-label context:create table qualtrics
 --comment: example comment
 create or replace table DEMO_DB.ALF_LIQUIBASE.QUALTRICS
@@ -9,8 +8,6 @@ create or replace table DEMO_DB.ALF_LIQUIBASE.QUALTRICS
     SURVEY_ID      VARCHAR(50),
     EXECUTION_FLAG BOOLEAN
 );
-
-
 
 --changeset a.loic:2 labels:example-label
 --comment: insert a
